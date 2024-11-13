582086047acd8940269a80d97dc011fd22956f44
Minata P Mahaarta <minataputra@gmail.com>
2024-11-13 15:01:54 +0700

    Initial Commit

---
+++ iosTestMinata/iosTestMinataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTestMinataApp.swift
+//  iosTestMinata
+//
+//  Created by Minata on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iosTestMinataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
